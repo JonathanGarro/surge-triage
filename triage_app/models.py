8288990b37eb706e307a245716818afb2acec128
Jonathan Garro <jonathan.garro@gmail.com>
2023-09-26 10:23:33 -0400

    alert class

--- triage_app/models.py
+++ triage_app/models.py
@@ -16,3 +16,24 @@ class User(db.Model):
     last_name = db.Column(db.String)
     email = db.Column(db.String)
     slack_id = db.Column(db.String)
+
+class Alert(db.Model):
+    __tablename__ = "alerts"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    alert_id = db.Column(db.Integer)
+    message = db.Column(db.String)
+    molnix_id = db.Column(db.Integer)
+    molnix_created_at = db.Column(db.String)
+    opens = db.Column(db.String)
+    closes = db.Column(db.String)
+    start = db.Column(db.String)
+    end = db.Column(db.String)
+    region = db.Column(db.String)
+    language = db.Column(db.String)
+    sector = db.Column(db.String)
+    modality = db.Column(db.String)
+    scope = db.Column(db.String)
+    rotation = db.Column(db.String)
+    event_name = db.Column(db.String)
+    event_id = db.Column(db.String)
