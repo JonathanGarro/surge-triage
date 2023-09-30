0d2f59144a0ee523d844ff4fc6ffec011851ee99
Jonathan Garro <jonathan.garro@gmail.com>
2023-09-30 19:08:00 -0400

    add twilio sid and token

--- triage_app/config.py
+++ triage_app/config.py
@@ -7,3 +7,5 @@ class Config:
     SESSION_PERMANENT = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TWILIO_SID = os.environ.get('TWILIO_SID')
+    TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
