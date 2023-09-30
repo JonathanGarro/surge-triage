14f11b96013aa1195fc820624860ab8f1835a57d
Jonathan Garro <jonathan.garro@gmail.com>
2023-09-30 19:07:46 -0400

    add bcrypt and login utils

--- triage_app/__init__.py
+++ triage_app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager, current_user
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from triage_app.config import Config
 import os
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
+login_manager.login_view = 'users.login' 
+login_manager.login_message_category = 'danger'
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
