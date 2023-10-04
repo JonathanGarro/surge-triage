7e419256f361a6d6c543bf1f2c75d1f882790d85
Jonathan Garro <jonathan.garro@gmail.com>
2023-10-04 15:21:17 -0400

    datatable js config

--- triage_app/static/js/scripts.js
+++ triage_app/static/js/scripts.js
@@ -1,3 +1,40 @@
 window.setTimeout(function() {
     $("#alert").fadeTo(500, 0) 
 }, 6000);
+
+// $(document).ready(function () {
+//     $('#alert-table').DataTable({
+//         order: [[5, 'desc']],
+//         dom: 'frtipB',
+//         buttons: [
+//             'copy', 'csv'
+//         ],
+//         columnDefs: [{
+//             target: 6,
+//             visible: false,
+//             searchable: true,
+//         },
+//         {
+//             target: 7, 
+//             visible: false,
+//             searchable: true
+//         }],
+//     });
+// });
+
+$(document).ready(function () {
+    $('#alert-table').DataTable({
+        order: [[0, 'desc']], // Sort by column index 5 (reverse chronological order)
+        dom: 'frtipB',
+        buttons: [
+            'copy', 'csv'
+        ],
+        columnDefs: [
+            {
+                targets: [0], // Target the first column (column index 0)
+                visible: false, // Hide the first column
+                searchable: false, // Make it not searchable
+            },
+        ],
+    });
+});
