06545dba937798c8ea90ae5276b35eff37d6b1f7
Jonathan Garro <jonathan.garro@gmail.com>
2023-10-04 15:20:39 -0400

    mail config

--- triage_app/config.py
+++ triage_app/config.py
@@ -1,7 +1,16 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     DEBUG = True
+    HOST_EMAIL = os.environ.get('HOST_EMAIL')
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')   
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
@@ -9,3 +18,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TWILIO_SID = os.environ.get('TWILIO_SID')
     TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+    
